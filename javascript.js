218510c6e800da224cc5c4d42572930bcf651a90
= <=>
2020-09-23 14:10:29 +0200

    subo probando de leer json local y no funciona

---
+++ javascript.js
@@ -0,0 +1,56 @@
+
+var mydata = JSON.parse(data);
+
+//const arrayObj = [{"id":1,"fecha":"2012/04/30 10:14:23","titulo":"entrada1","descripcion":"entrada1 descripcion"},{"id":2,"fecha":"2020/09/22 16:28:23","titulo":"entrada2","descripcion":"entrada2 descripcion"}];
+const arrayObj = mydata;
+
+for (var i = 0; i < arrayObj.length; i++) {
+    alert(
+        "Id: " + arrayObj[i].id +
+        "\nFecha: " + arrayObj[i].fecha +
+        "\nTitulo: " + arrayObj[i].titulo +
+        "\nDescripcion: " + arrayObj[i].descripcion
+    );
+}
+
+//Funcion para generar objeto con titulo y cuerpo
+function generarObjEntradaBlog() {
+    var entradaBlog = new Object();
+    entradaBlog.titulo = document.getElementById("titulo").value;
+    entradaBlog.cuerpo = document.getElementById("cuerpo").value;
+    return entradaBlog;
+}
+
+function generarJsonEntradaBlog() {
+
+}
+
+//Aqui controlamos que se hayan introducido los datos, devuelve "" si se han introducido los 2 datos, de lo contrario devuelve un string con los fallos
+function controlDeDatos() {
+    var strErrores = "";
+
+    //Si no han introducido el titulo sera error
+    if (document.getElementById("titulo").value == "") {
+        strErrores += "Titulo no introducido\n";
+    }
+
+    if (document.getElementById("cuerpo").value == "") {
+        strErrores += "Cuerpo no introducido\n";
+    }
+
+    return strErrores;
+}
+
+//Funcion para añadir entrada
+function anadirEntrada() {
+    alert(llistatHotels);
+
+    var datosCompletados = controlDeDatos();
+    if (datosCompletados == "") {
+
+    }
+    else {
+        alert("Faltan datos por completar:\n" + datosCompletados);
+    }
+}
+
